3842bbdcbb99ee96f8a4ab362f2af5a28336e499
andreyhgl <andrey.hoglund@gmail.com>
2025-11-11 11:26:12 +0100

    do not add aliases.sh on ZSH-system

--- install.sh
+++ install.sh
@@ -3,13 +3,25 @@
 # check which shell is being used
 if [ -f "${HOME}/.bashrc" ]; then
 	SH=${HOME}/.bashrc
+	IS_BASH=true
 else
 	SH="${HOME}/.zshrc"
+	IS_BASH=false
 fi
 
 echo '# ~~~~~~~~~ andreys dotfiles config ~~~~~~~~~ #' >> $SH
 
+# skip aliases.sh on zsh
+SKIP_FILE="shell/aliases.sh"
+
 for file in shell/*; do
+
+  # Skip if this is the bash-only file and we're not using bash
+  if [ "$IS_BASH" = false ] && [ "$file" = "$SKIP_FILE" ]; then
+        echo "Skipping $file"
+        continue
+  fi
+
 	echo "${file}"
 	echo "source ${HOME}/.dotfiles/${file}" >> $SH
 done
