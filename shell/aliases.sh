ce68393b0a1fbb1c8d8b1dfd06a3283ab4c05d5c
andreyhgl <andrey.hoglund@gmail.com>
2026-03-16 09:01:02 +0100

    add EDGER image alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,6 +3,9 @@ alias ll='ls -lhov --group-directories-first --color'
 alias la='ls -alhov --group-directories-first --color'
 alias du='du -sh'
 
+# Nextflow
+alias nl='nextflow log'
+
 # squeue output, %[number][flag], %.10i
 # %i = Job ID, %P = Partition, %j = Job name (full), %u = User, %T = State
 # %M = Time used, %l = Time limit, %D = Nodes, %R = Reason/Nodelist
@@ -12,3 +15,4 @@ alias jobinfo='squeue -u $USER'
 export RNASEQ_SIF="library://andreyhgl/singularity-r/rnaseq"
 export GO_SIF="library://andreyhgl/singularity-r/gene-ontology"
 export METH_SIF="library://andreyhgl/singularity-r/methylome"
+export EDGER_SIF="library://andreyhgl/r/edger"
